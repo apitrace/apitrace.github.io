21406eba131bfceaccea1748ffdde96fdb9080ca
James Benton <jbenton@vmware.com>
2012-08-10 14:44:41 +0100

    Cleanup javascript / css files.

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,31 +1,61 @@
 (function($){  
   $(function(){
-    
-    $(document).foundationAlerts();
-    $(document).foundationAccordion();
-    $(document).tooltips();
-    $('input, textarea').placeholder();
-    
-    
-    
-    $(document).foundationButtons();
-    
-    
-    
-    $(document).foundationNavigation();
-    
-    
-    
-    
-    
-    
-    
-    
-    // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
-    // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'left'});
-    // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'left'});
-    // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'left'});
-    // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'left'});
+    $.waypoints.settings.scrollThrottle = 30;
+
+    $('#nav').waypoint(function(event, direction) {
+      $(this).parent().toggleClass('sticky', direction === "down");
+      event.stopPropagation();
+    });
+
+    // The same for all waypoints
+    $('body').delegate('section', 'waypoint.reached', function(event, direction) {
+      var $active = $(this);
+
+      if (direction === "up") {
+        $active = $active.prev();
+      }
+      if (!$active.length) $active = $active.end();
+      
+      $('.section-active').removeClass('section-active');
+      $active.addClass('section-active');
+      
+      $('.active').removeClass('active');
+
+      var activeID = $active.attr('id');
+      if (activeID == 'source') {
+        activeID = 'source-acquiring';
+      }
+      $('a[href=#'+activeID+']').parent().addClass('active');
+    });
+
+    $('section').waypoint({ offset: '10%' });
+
+    // http://www.zachstronaut.com/posts/2009/01/18/jquery-smooth-scroll-bugs.html
+    var scrollElement = 'html, body';
+    $('html, body').each(function () {
+      var initScrollTop = $(this).attr('scrollTop');
+      $(this).attr('scrollTop', initScrollTop + 1);
+      if ($(this).attr('scrollTop') == initScrollTop + 1) {
+        scrollElement = this.nodeName.toLowerCase();
+        $(this).attr('scrollTop', initScrollTop);
+        return false;
+      }    
+    });
+
+    $("a[href^='#']").click(function(event) {
+      event.preventDefault();
+      
+      var $this = $(this),
+      target = this.hash,
+      $target = $(target);
+      
+      $(scrollElement).stop().animate({
+        'scrollTop': $target.offset().top
+      }, 500, 'swing', function() {
+        window.location.hash = target;
+      });
+      
+    });
   });
   
 })(jQuery);
